0aea5fa3328850f143664dfb31a74ffc8bc6ec42
Daffa Abipraya <daffaabhiprayaputra@gmail.com>
2024-06-16 15:34:12 +0700

    finished exercise 3.13 - 3.14

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,84 +1,67 @@
+// setup express, morgan, cors, dotenv, mongoose
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+require('dotenv').config()
+const Person = require('./models/person')
 
 const app = express()
+
+// use cors, json, static
 app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
-// log request body
+// morgan setup
 app.use((req, res, next) => {
     req.parsedBody = JSON.stringify(req.body)
     next()
 })
-
 morgan.token('body', (req) => req.parsedBody)
-
 const customFormat = ':method :url :status :res[content-length] - :response-time ms :body'
-
 app.use(morgan(customFormat))
 
-// path: /info
+
+// ---------- routes ----------
+
+
+// get info
 app.get('/info', (req, res) => {
     const date = new Date()
-    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+    })
 })
 
-// path: /api/persons
+// get all persons
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(person => {
+        res.json(person)
+    })
 })
 
-// path: /api/persons/:id
+// get person by id
 app.get('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    const person = persons.find(person => person.id.toString() === id)
-
-    person
-        ? res.json(person)
-        : res.status(404).end()
+    Person.findById(req.params.id).then(person => {
+        res.json(person)
+    })
 })
 
 // delete person
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+    Person.findByIdAndDelete(req.params.id).then(() => {
+        res.status(204).end()
+    })
 })
 
 // generate id using math random from 1 to 1000000
-const generateId = () => {
-    const id = persons.length > 0
-        ? Math.floor(Math.random() * 1000000)
-        : 0
-    console.log(`random generated id: ${id}`)
-    return id
-}
+// const generateId = () => {
+//     const id = persons.length > 0
+//         ? Math.floor(Math.random() * 1000000)
+//         : 0
+//     console.log(`random generated id: ${id}`)
+//     return id
+// }
 
 // add person
 app.post('/api/persons', (req, res) => {
@@ -92,26 +75,26 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    // check if name already exists
-    const nameExists = persons.find(person => person.name === body.name)
-    if (nameExists) {
-        console.log(`${body.name} already exists`)
-        return res.status(400).json({
-            error: `${body.name} already exists`
-        })
-    }
+    // TODO check if name already exists
+    // const nameExists = persons.find(person => person.name === body.name)
+    // if (nameExists) {
+    //     console.log(`${body.name} already exists`)
+    //     return res.status(400).json({
+    //         error: `${body.name} already exists`
+    //     })
+    // }
 
-    // create new person
-    const person = {
+    // create new person using mongoose model
+    const person = new Person({
         name: body.name,
         number: body.number,
-        id: generateId()
-    }
+        // id: generateId()
+    })
 
-    // add person to persons
-    persons = persons.concat(person)
-
-    res.json(person)
+    // add person
+    person.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 })
 
 // unknown endpoint
@@ -121,8 +104,12 @@ const unknownEndpoint = (req, res, next) => {
 
 app.use(unknownEndpoint)
 
+
+// ----------------------------
+
+
 // port 3001
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`server running on port ${PORT}`)
 })
